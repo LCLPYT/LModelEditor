cb2a7aa6406d757aa822315c925a5146b529945c
LCLP <lukas@lclpnet.work>
2021-01-15 23:06:26 +0100

    textures loading

--- src/ts/loader.ts
+++ src/ts/loader.ts
@@ -1,4 +1,4 @@
-import { Vector2, Vector3 } from "three";
+import { Texture, Vector2, Vector3 } from "three";
 import { Cube } from "./data/Cube";
 import { CubeTexture } from "./data/CubeTexture";
 import { LModel } from "./data/LModel";
@@ -31,4 +31,59 @@ export function getDefaultModel(): LModel {
     renderer.cubes.push(cube);
     model.renderers.push(renderer);
     return model;
+}
+
+export type RemoteImage = string | {
+    src: string;
+    /** @defaultvalue "anonymous" */
+    crossOrigin?: string | null;
+    referrerPolicy?: string;
+};
+  
+export type TextureCanvas = HTMLCanvasElement | OffscreenCanvas;
+export type TextureSource = HTMLImageElement | HTMLVideoElement | ImageBitmap | TextureCanvas;
+
+export function isTextureSource(value: unknown): value is TextureSource {
+    return value instanceof HTMLImageElement ||
+        value instanceof HTMLVideoElement ||
+        value instanceof HTMLCanvasElement ||
+        (typeof ImageBitmap !== "undefined" && value instanceof ImageBitmap) ||
+        (typeof OffscreenCanvas !== "undefined" && value instanceof OffscreenCanvas);
+}
+
+export async function loadImage(source: RemoteImage): Promise<HTMLImageElement> {
+    const image = document.createElement("img");
+    return new Promise((resolve, reject) => {
+        image.onload = (): void => resolve(image);
+        image.onerror = reject;
+        image.crossOrigin = "anonymous";
+        if (typeof source === "string") {
+            image.src = source;
+        } else {
+            if (source.crossOrigin !== undefined) {
+                image.crossOrigin = source.crossOrigin;
+            }
+            if (source.referrerPolicy !== undefined) {
+                image.referrerPolicy = source.referrerPolicy;
+            }
+            image.src = source.src;
+        }
+    });
+}
+
+export function loadSkinToCanvas(canvas: TextureCanvas, image: TextureSource): void {
+	const context = canvas.getContext("2d")!;
+	canvas.width = image.width;
+	canvas.height = image.height;
+	context.clearRect(0, 0, image.width, image.height);
+	context.drawImage(image, 0, 0, canvas.width, canvas.height);
+}
+
+export function loadSkin(source: TextureSource | RemoteImage | null, canvas: HTMLCanvasElement, texture: Texture): void | Promise<void> {
+    if (isTextureSource(source)) {
+        loadSkinToCanvas(canvas, source);
+        texture.needsUpdate = true;
+    } else {
+        return loadImage(source).then(image => loadSkin(image, canvas, texture));
+    }
 }
--- src/ts/scene.ts
+++ src/ts/scene.ts
@@ -2,7 +2,7 @@ import * as THREE from 'three';
 import { Cube } from './data/Cube';
 import { LModel } from './data/LModel';
 import { ModelRenderer } from './data/ModelRenderer';
-import { convertModelToJson, fetchJson, getDefaultModel, loadModelFromJson } from './loader';
+import { convertModelToJson, fetchJson, getDefaultModel, loadModelFromJson, loadSkin } from './loader';
 import { setSelectableObjects, setSelectedObjects } from './render';
 
 export let scene: THREE.Scene;
@@ -38,32 +38,41 @@ export function populateScene() {
     scene.add(ambientLight);
     
     const light = new THREE.PointLight(0xffffff, 0.7);
-    // light.position.set(5, 7, 10);
     light.position.set(0, 1, 0);
     camera.add(light);
 
     scene.add(new THREE.GridHelper(16, 16, 0x888888, 0x444444));
 
     // loadModel(getDefaultModel());
-    fetchJson('resource/creeper.json', (status, response) => {
+
+    let modelName = 'boat';
+
+    fetchJson(`resource/${modelName}.json`, (status, response) => {
         if(status !== 200) {
             console.error("An error occured. HTTP " + status);
             return;
         }
 
+        const canvas = document.createElement("canvas");
+		const texture = new THREE.Texture(canvas);
+		texture.magFilter = THREE.NearestFilter;
+        texture.minFilter = THREE.NearestFilter;
+
+        loadSkin(`resource/${modelName}.png`, canvas, texture);
+
         let model = <LModel> response;
-        loadModel(model);
+        loadModel(model, texture);
     });
 }
 
-function loadModel(model: LModel) {
+function loadModel(model: LModel, texture: THREE.Texture) {
     if(model.initialTranslation === undefined) model.initialTranslation = new THREE.Vector3(0, -1.5, 0);
-    model.renderers.forEach(r => addRenderer(r, model));
+    model.renderers.forEach(r => addRenderer(r, model, texture));
 }
 
-function addRenderer(renderer: ModelRenderer, model: LModel) {
-    renderer.children.forEach(r => addRenderer(r, model));
-    renderer.cubes.forEach(cube => addCube(cube, renderer, model));
+function addRenderer(renderer: ModelRenderer, model: LModel, texture: THREE.Texture) {
+    renderer.children.forEach(r => addRenderer(r, model, texture));
+    renderer.cubes.forEach(cube => addCube(cube, renderer, model, texture));
 
     const rotPoint = new THREE.Mesh(new THREE.SphereGeometry(0.1, 32, 32), new THREE.MeshBasicMaterial({ color: 0xff0000 }));
     rotPoint.position.set(
@@ -76,11 +85,19 @@ function addRenderer(renderer: ModelRenderer, model: LModel) {
     scene.add(rotPoint);
 }
 
-function addCube(cube: Cube, renderer: ModelRenderer, model: LModel) {
+function addCube(cube: Cube, renderer: ModelRenderer, model: LModel, texture: THREE.Texture) {
     const geometry = new THREE.BoxGeometry(cube.dimensions.x, cube.dimensions.y, cube.dimensions.z);
+    setUVs(geometry, cube.texture.offsetX, cube.texture.offsetY, cube.dimensions.x, cube.dimensions.y, cube.dimensions.z, cube.texture.width, cube.texture.height);
+
     const material = new THREE.MeshStandardMaterial({
-        color: 0xaaaaaa
+        map: texture,
+        side: THREE.DoubleSide,
+        transparent: true,
+        alphaTest: 1e-5
     });
+    // const material = new THREE.MeshStandardMaterial({
+    //     color: 0xaaaaaa
+    // });
     const mesh = new THREE.Mesh(geometry, material);
     mesh.position.set(
         // axis direction  |  cube center to begin vertex  | offset correction
@@ -113,6 +130,40 @@ function addCube(cube: Cube, renderer: ModelRenderer, model: LModel) {
 
     scene.add(mesh);
     objects.push(mesh);
+}
+
+/**
+ * Source: https://github.com/bs-community/skinview3d/blob/f5c7692c8d1b63631ab016d7873ddbdf12326cbc/src/model.ts
+ */
+function setUVs(box: THREE.BoxGeometry, u: number, v: number, width: number, height: number, depth: number, textureWidth: number, textureHeight: number): void {
+	const toFaceVertices = (x1: number, y1: number, x2: number, y2: number) => [
+		new THREE.Vector2(x1 / textureWidth, 1.0 - y2 / textureHeight),
+		new THREE.Vector2(x2 / textureWidth, 1.0 - y2 / textureHeight),
+		new THREE.Vector2(x2 / textureWidth, 1.0 - y1 / textureHeight),
+		new THREE.Vector2(x1 / textureWidth, 1.0 - y1 / textureHeight)
+    ];
+    
+	const top = toFaceVertices(u + depth, v, u + width + depth, v + depth);
+	const bottom = toFaceVertices(u + width + depth, v, u + width * 2 + depth, v + depth);
+	const left = toFaceVertices(u, v + depth, u + depth, v + depth + height);
+	const front = toFaceVertices(u + depth, v + depth, u + width + depth, v + depth + height);
+	const right = toFaceVertices(u + width + depth, v + depth, u + width + depth * 2, v + height + depth);
+    const back = toFaceVertices(u + width + depth * 2, v + depth, u + width * 2 + depth * 2, v + height + depth);
+    
+	box.faceVertexUvs[0] = [
+		[right[3], right[0], right[2]],
+		[right[0], right[1], right[2]],
+		[left[3], left[0], left[2]],
+		[left[0], left[1], left[2]],
+		[top[3], top[0], top[2]],
+		[top[0], top[1], top[2]],
+		[bottom[0], bottom[3], bottom[1]],
+		[bottom[3], bottom[2], bottom[1]],
+		[front[3], front[0], front[2]],
+		[front[0], front[1], front[2]],
+		[back[3], back[0], back[2]],
+		[back[0], back[1], back[2]]
+	];
 }
 
 export function setSelected(obj: THREE.Object3D, reset: boolean) {
